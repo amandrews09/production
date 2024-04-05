c74d3c123ef2ae3bc8a20ebd002f7fb297dc3444
Josh-Manley <joshmanley45@gmail.com>
2024-04-04 19:09:01 -0600

    link edits

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,9 +2,7 @@ module.exports = {
     format_time: (date) => {
       return date.toLocaleTimeString();
     },
-    format_date: (date) => {
-      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
-        new Date(date).getFullYear() + 5
-      }`;
+    format_date: date => {
+      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
     },
   };
