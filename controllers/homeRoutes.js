f1ce9a2e146543d48c0ec6582114f1f4d3c8c5cd
Mallory Howard <mallory1313@msn.com>
2024-04-08 18:12:19 -0600

    added a bunch of timing stuff

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,31 @@
 const router = require('express').Router();
 const { Product, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
+const moment = require('moment');
+
+// router.get('/products', async (req, res) => {
+//   try {
+//     const productData = await Product.findAll({
+//       include: [
+//         {
+//           model: User,
+//           attributes: ['name'],
+//         },
+//         {
+//           model: Comment,
+//           include: [Product],
+//           attributes: ['text'],
+//         },
+//       ],
+//     });
+
+//     // const product = productData.map(Product => Product.get({ plain: true }));
+
+//    res.status(200).json(productData)
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 router.get('/products', async (req, res) => {
   try {
@@ -18,9 +43,21 @@ router.get('/products', async (req, res) => {
       ],
     });
 
-    // const product = productData.map(Product => Product.get({ plain: true }));
+    const productsFormatted = productData.map(product => {
+      const productPlain = product.get({ plain: true });
+      // if (productPlain.selectedDate) {
+      //   productPlain.selectedDate = moment(productPlain.selectedDate).format('ddd MMM DD YYYY');
+      // }
+      if (productPlain.startTime) {
+        productPlain.startTime = moment(productPlain.startTime).format('ddd MMM DD YYYY HH:mm:ss');
+      }
+      if (productPlain.stopTime) {
+        productPlain.stopTime = moment(productPlain.stopTime).format('ddd MMM DD YYYY HH:mm:ss');
+      }
+      return productPlain;
+    });
 
-   res.status(200).json(productData)
+    res.status(200).json(productsFormatted);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -42,7 +79,13 @@ router.get('/data', async (req, res) => {
       ],
     });
     // Serialize data so the template can read it
-    const products = await productData.map(Product => Product.get({ plain: true }));
+    const products = productData.map(product => {
+      const productPlain = product.get({ plain: true });
+      // productPlain.selectedDate = productPlain.selectedDate ? moment(productPlain.selectedDate).format('ddd MMM DD YYYY') : null;
+      productPlain.startTime = productPlain.startTime ? moment(productPlain.startTime).format('ddd MMM DD YYYY HH:mm:ss') : null;
+      productPlain.stopTime = productPlain.stopTime ? moment(productPlain.stopTime).format('ddd MMM DD YYYY HH:mm:ss') : null;
+      return productPlain;
+    });
 
     const currentUser = await User.findByPk(req.session.user_id);
 
@@ -73,8 +116,10 @@ router.get('/product/:id', async (req, res) => {
           model: User,
           attributes: ['name', 'id'],
         },
-        {model:Comment, include:[User],
-        attributes:['text']}
+        {
+          model: Comment, include: [User],
+          attributes: ['text']
+        }
       ],
     });
     const product = productData.get({ plain: true });
@@ -131,19 +176,25 @@ router.get('/warning', (req, res) => {
 
 router.get('/comment/:id', withAuth, async (req, res) => {
   const productData = await Product.findByPk(req.params.id,
-    {include: [User,
-      {model:Comment, attributes:['text'], include:[User],
-  }]});
+    {
+      include: [User,
+        {
+          model: Comment, attributes: ['text'], include: [User],
+        }]
+    });
   const productDataPlain = productData.get({ plain: true });
-  res.render('comment', {productDataPlain, logged_in: req.session.logged_in, userId: req.session.user_id});
+  res.render('comment', { productDataPlain, logged_in: req.session.logged_in, userId: req.session.user_id });
 })
 router.get('/edit/:id', withAuth, async (req, res) => {
   const productData = await Product.findByPk(req.params.id,
-    {include: [User,
-      {model:Comment, attributes:['text'], include:[User],
-  }]});
+    {
+      include: [User,
+        {
+          model: Comment, attributes: ['text'], include: [User],
+        }]
+    });
   const productDataPlain = productData.get({ plain: true });
-  res.render('edit', {productDataPlain, logged_in: req.session.logged_in, userId: req.session.user_id});
+  res.render('edit', { productDataPlain, logged_in: req.session.logged_in, userId: req.session.user_id });
 })
 
 module.exports = router;
